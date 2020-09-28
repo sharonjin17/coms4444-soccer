618516cb46c172c1b0dccc8913c74d4b4236fa57
AumUpadhyay <aumspeaks@gmail.com>
2020-09-27 22:26:58 -0700

    Update Player.java

--- src/g4/Player.java
+++ src/g4/Player.java
@@ -75,6 +75,7 @@ public class Player extends sim.Player {
 			}
 			else {
 				reallocatedPlayerGames = reallocateLeapFrog(round, gameHistory, playerGames, opponentGamesMap);
+				System.out.println("HERE2");
 			}
 		}
 		//List<Game> leapfrogGames = reallocateLeapFrog(round, gameHistory, playerGames, opponentGamesMap);
